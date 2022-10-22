5916a769b6929282836bea8e85dc17eaedb5659a
alexsks65536 <megbyte@mail.ru>
2022-10-22 23:59:22 +0300

    lesson_08. Формирование URL-адресов в шаблонах

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -3,38 +3,43 @@ from django.shortcuts import render, redirect
 
 from .models import *
 
-menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+menu = [{'title': 'О сайте', 'url_name': 'about'},
+        {'title': 'Добавить статью', 'url_name': 'addpage'},
+        {'title': 'Обратная связь', 'url_name': 'contact'},
+        {'title': 'Войти', 'url_name': 'login'},
+]
 
 
 def index(request):  # HttpRequest
     posts = Women.objects.all()  # Подключаемся к БД Women, считываем все значения и передаем в шаблон переменные
-    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница'
+    }
+    return render(request, 'women/index.html', context=context)
 
 
 def about(request):  # HttpRequest
     return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
 
 
-def categories(request, catid):
-    if(request.GET):
-        print(request.GET)
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
+def addpage(request):
+    return HttpResponse('Добавление статьи')
 
 
-def archive(request, year):
-    if int(year) > 2020:
-        # raise Http404()
-        return redirect('home', permanent=True)  # постоянный редирект
-        # return redirect('/')
+def contact(request):
+    return HttpResponse('Обратная связь')
 
-    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
+def login(request):
+    return HttpResponse('Авторизация')
 
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
 
-def test(request):
-    return HttpResponse("<h2>Это тестовая страница</h2>")
-
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
 
