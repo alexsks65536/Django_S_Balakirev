64bb632edc11b21a4a9896c0c9e0e3f4160ac98e
alexsks65536 <megbyte@mail.ru>
2022-10-23 23:31:36 +0300

    lesson_09. Создание связей между моделями через класс ForeignKey

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -12,10 +12,12 @@ menu = [{'title': 'О сайте', 'url_name': 'about'},
 
 def index(request):  # HttpRequest
     posts = Women.objects.all()  # Подключаемся к БД Women, считываем все значения и передаем в шаблон переменные
+    cats = Category.objects.all()
     context = {
         'posts': posts,
+        'cats': cats,
         'menu': menu,
-        'title': 'Главная страница'
+        'title': 'Отображение по рубрикам',
     }
     return render(request, 'women/index.html', context=context)
 
@@ -42,4 +44,22 @@ def pageNotFound(request, exception):
 
 def show_post(request, post_id):
     return HttpResponse(f"Отображение статьи с id = {post_id}")
+
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(
+        cat_id=cat_id)  # Подключаемся к БД Women, считываем все значения и передаем в шаблон переменные
+    cats = Category.objects.all()
+
+    if len(posts) == 0:  # если нет такой категории, генерирует "Страница на найдена"
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Отображение по рубрикам',
+        'cat_selected': cat_id,
+    }
+    return render(request, 'women/index.html', context=context)
 
