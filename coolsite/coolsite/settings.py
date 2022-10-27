e40a640a9ba2b00ecaa418c41ba509123fdeec92
alexsks65536 <megbyte@mail.ru>
2022-10-27 23:52:13 +0300

    lesson_12. Добавляем слаги (slug) к URL-адресам

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -129,4 +129,5 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
