5ff6d8b2998ddcee6300102bf8c16b28d58cfa6d
alexsks65536 <megbyte@mail.ru>
2022-10-28 19:26:05 +0300

    Использование форм, не связанных с моделями

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 
+from .forms import AddPostForm
 from .models import *
 
 """
@@ -10,7 +11,7 @@ menu = [{'title': 'О сайте', 'url_name': 'about'},
         {'title': 'Добавить статью', 'url_name': 'addpage'},
         {'title': 'Обратная связь', 'url_name': 'contact'},
         {'title': 'Войти', 'url_name': 'login'},
-]
+        ]
 
 
 def index(request):  # HttpRequest
@@ -29,7 +30,22 @@ def about(request):  # HttpRequest
 
 
 def addpage(request):
-    return HttpResponse('Добавление статьи')
+    """
+    Форма добавления статьи на сайт
+    """
+    if request.method == 'POST':
+        form = AddPostForm(request.POST)
+        if form.is_valid():
+            try:
+                Women.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, 'Ошибка добавления поста')
+
+            print(form.cleaned_data)
+    else:
+        form = AddPostForm()
+    return render(request, 'women/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
 
 
 def contact(request):
@@ -77,5 +93,3 @@ def show_category(request, cat_slug):
         'cat_selected': cat_slug,
     }
     return render(request, 'women/index.html', context=context)
-
-
